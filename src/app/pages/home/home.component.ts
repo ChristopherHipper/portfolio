0f0fc0c75df1650f79dcb6c4019362244086caef
Christopher Hipper <hipper.christopher@gmail.com>
2026-01-19 20:34:35 +0100

    implement home sections and start with about me

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { AboutMeSectionComponent } from "./sections/about-me-section/about-me-section.component";
+import { SkillsSectionComponent } from "./sections/skills-section/skills-section.component";
+import { ProjectsSectionComponent } from "./sections/projects-section/projects-section.component";
+import { TestimonialsSectionComponent } from "./sections/testimonials-section/testimonials-section.component";
+import { ContactSectionComponent } from "./sections/contact-section/contact-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AboutMeSectionComponent, SkillsSectionComponent, ProjectsSectionComponent, TestimonialsSectionComponent, ContactSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/pages/home/sections/about-me-section/about-me-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../../shared/button/button.component";
+
+@Component({
+  selector: 'app-about-me-section',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './about-me-section.component.html',
+  styleUrl: './about-me-section.component.scss'
+})
+export class AboutMeSectionComponent {
+
+}
