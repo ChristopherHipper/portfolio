c67d0d5d002cb0a43bcd30aecb786d9e6b98a677
Christopher Hipper <hipper.christopher@gmail.com>
2026-01-20 20:31:45 +0100

    update about me section

--- src/app/pages/home/sections/about-me-section/about-me-section.component.ts
+++ src/app/pages/home/sections/about-me-section/about-me-section.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonComponent } from "../../../../shared/button/button.component";
 
 @Component({
   selector: 'app-about-me-section',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,],
   templateUrl: './about-me-section.component.html',
   styleUrl: './about-me-section.component.scss'
 })
 export class AboutMeSectionComponent {
-
+  titles:string[] = ['located in Germany', 'ready to work remote'];
 }
