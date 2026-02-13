97082304f90864bbebc29030fcca28cb4fe8a54c
Christopher Hipper <hipper.christopher@gmail.com>
2026-02-13 21:07:22 +0100

    change hotlistener to intersectionObserver

--- src/app/shared/header/nav/nav.component.ts
+++ src/app/shared/header/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, HostBinding } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,17 +7,46 @@ import { Component, HostListener, HostBinding } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
-export class NavComponent {
-  navList:string[] = ['Why me', 'Skills', 'Projects', 'Contact']
+export class NavComponent implements AfterViewInit, OnDestroy {
+  navList: string[] = ['Why me', 'Skills', 'Projects', 'Contact']
   activeLink: string = '';
+  private observer!: IntersectionObserver;
+  options = {
+      root: null,
+      threshold: 0.7
+    };
 
-  setActiveClass(item:string){
+  setActiveClass(item: string) {
     this.activeLink = item;
   }
 
-  @HostListener('window:scroll', ['$event']) onScrollEvent($event:any){
-    console.log('scrolling');
-    
+
+  ngAfterViewInit(): void {
+    this.initIntersectionObserver();
+    this.getSection();
   }
-  
+
+  initIntersectionObserver() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            this.activeLink = entry.target.id
+        }
+      });
+    }, this.options);
+  }
+
+  getSection(){
+    this.navList.forEach(id => {
+      const section = document.getElementById(id);
+      if (section) {
+        this.observer.observe(section);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
+  }
+
 }
